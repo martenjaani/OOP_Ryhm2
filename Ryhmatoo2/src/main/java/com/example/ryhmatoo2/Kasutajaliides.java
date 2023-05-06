21e38c07869e4b5def683d71f650fb1e549fe0ed
Marten Mathias Jaani <75548907+martenjaani@users.noreply.github.com>
2023-05-06 14:57:34 +0300

    disgusting alpha versioon

--- Ryhmatoo2/src/main/java/com/example/ryhmatoo2/Kasutajaliides.java
+++ Ryhmatoo2/src/main/java/com/example/ryhmatoo2/Kasutajaliides.java
@@ -15,7 +15,17 @@ import javafx.scene.text.Text;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static com.example.ryhmatoo2.Mäng.kumbVõidab;
+import static com.example.ryhmatoo2.Mäng.väljastaKaardid;
+
 public class Kasutajaliides extends Application {
+
+    static int mängijaVõitudeArv = 0;
+    static int arvutiVõitudeArv = 0;
 
     public static void main(String[] args) {
         launch(args);
@@ -24,17 +34,44 @@ public class Kasutajaliides extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        BorderPane juur=new BorderPane();
+        HBox juur=new HBox();
 
         Button juhendiNupp=new Button("JUHEND!");
 
-        juhendiNupp.setOnMouseClicked(event -> {
+        juhendiNupp.setOnMouseClicked(event -> näitaJuhendit());
 
-            Stage juhendiAken=new Stage();
+        Button mängiNupp=new Button("MÄNGI!");
 
-            String tekst="""
+        mängiNupp.setOnMouseClicked(event -> Mängi());
+
+        juur.getChildren().addAll(juhendiNupp,mängiNupp);
+
+        Scene scene = new Scene(juur);
+        scene.setFill(Color.ORANGE);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static StackPane kaartJoonis(Kaart kaart){
+        Rectangle bg=new Rectangle(50,100);
+        bg.setArcHeight(20);
+        bg.setArcWidth(20);
+        bg.setFill(Color.WHITE);
+
+        Text tekst=new Text(kaart.getTugevus()+" "+kaart.getMast());
+        return new StackPane(bg,tekst);
+
+    }
+
+    public static void näitaJuhendit(){
+        Stage juhendiAken=new Stage();
+
+        String tekst="""
             Mängu eesmärgiks on valida kahe juhusliku pokkerikäe vahel. Pärast valiiku tegemist väljastatakse 5 ühiskaarti ja öeldakse kumb kätest võitis. Mängija võidu korral suurendatakse mängija punkte ühe võrra. Vastasel juhul suurenevad arvuti punktid. Kui käed olid sama tugevad, siis punktid jäävad samaks. Kaartide tugevuste arvestamisel vaadatakse maksimaalselt 5 kaarti korraga.
+            
             Pokkerikäte tugevusjärjestus (alates tugevaimast):
+            
             Mastirida - 5 järjestikust kaarti, mis on samast mastist
             Nelik - 4 sama tugevusega kaarti
             Maja - kolmik ja paar
@@ -47,46 +84,141 @@ public class Kasutajaliides extends Application {
             
             \s""";
 
-            TextArea juhend=new TextArea(tekst);
-            juhend.setWrapText(true);
+        TextArea juhend=new TextArea(tekst);
+        juhend.setWrapText(true);
 
-            VBox juur1 =new VBox();
+        VBox juur1 =new VBox();
 
-            juur1.getChildren().add(juhend);
+        juur1.getChildren().add(juhend);
 
-            Button nupp=new Button("Saan aru!");
-            nupp.setOnMouseClicked(mouseEvent ->
-                    juhendiAken.close());
+        Button nupp=new Button("Saan aru!");
+        nupp.setOnMouseClicked(mouseEvent ->
+                juhendiAken.close());
 
-            juur1.getChildren().add(nupp);
+        juur1.getChildren().add(nupp);
 
-            Scene scene=new Scene(juur1);
-            juhendiAken.setScene(scene);
-            juhendiAken.show();
+        Scene scene=new Scene(juur1);
+        juhendiAken.setScene(scene);
+        juhendiAken.show();
+    }
+
+    public static void Mängi(){
+        Stage mänguLava=new Stage();
+
+        TilePane juur=new TilePane(10,10);
+        juur.setPrefRows(2);
+        juur.setPrefColumns(5);
+
+
+        Pakk mängukaardid = new Pakk();
+        mängukaardid.sega();
+
+        //Kaartide jagamine
+        List<Kaart> vasakKäsi = new ArrayList<>();
+        List<Kaart> paremKäsi = new ArrayList<>();
+        List<Kaart> ühiskaardid = new ArrayList<>();
+
+        mängukaardid.võtaKaart();
+        for (int i = 0; i < 2; i++) {
+            vasakKäsi.add(mängukaardid.võtaKaart());
+            paremKäsi.add(mängukaardid.võtaKaart());
+        }
+
+        mängukaardid.võtaKaart();
+        for (int i = 0; i < 3; i++) {
+            ühiskaardid.add(mängukaardid.võtaKaart());
+        }
+        for (int i = 0; i < 2; i++) {
+            mängukaardid.võtaKaart();
+            ühiskaardid.add(mängukaardid.võtaKaart());
+        }
+
+       // HBox ühiskaardidJoonis = new HBox();
+
+
+        HBox vasakKaardid=new HBox(10);
+        for (Kaart kaart : vasakKäsi) {
+            vasakKaardid.getChildren().add(kaartJoonis(kaart));
+        }
+        VBox vasakValik=new VBox(20);
+        vasakValik.getChildren().add(vasakKaardid);
+
+        Button vasakNupp=new Button("VASAK!");
+        vasakValik.getChildren().add(vasakNupp);
+
+
+        HBox paremKaardid=new HBox(10);
+        for (Kaart kaart : vasakKäsi) {
+            paremKaardid.getChildren().add(kaartJoonis(kaart));
+        }
+        VBox paremValik=new VBox(20);
+        paremValik.getChildren().add(paremKaardid);
+
+
+        Button paremNupp=new Button("PAREM!");
+        paremValik.getChildren().add(paremNupp);
+
+        juur.getChildren().addAll(vasakValik,paremValik);
+
+
+
+
+        SeisuKontroll vasak = new SeisuKontroll(vasakKäsi, ühiskaardid); //loodud isendil on väga palju kasulikke meetodeid, mida hakkame kasutama seisude võrdlemiseks
+        SeisuKontroll parem = new SeisuKontroll(paremKäsi, ühiskaardid);
+
+        vasakNupp.setOnMouseClicked(event -> {
+            vasakValik(vasak,parem);
+            for (Kaart kaart : ühiskaardid) {
+                juur.getChildren().add(kaartJoonis(kaart));
+            }
+        });
+        paremNupp.setOnMouseClicked(event ->{
+            paremValik(vasak,parem);
+            for (Kaart kaart : ühiskaardid) {
+                juur.getChildren().add(kaartJoonis(kaart));
+            }
         });
 
-        HBox ühiskaardid =new HBox();
-        ühiskaardid.getChildren().add(kaartJoonis(new Kaart("5",'♣')));
-
-        juur.setCenter(ühiskaardid);
-        juur.setTop(juhendiNupp);
 
         Scene scene=new Scene(juur);
-        scene.setFill(Color.ORANGE);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
+        mänguLava.setScene(scene);
+        mänguLava.show();
 
     }
 
-    public static StackPane kaartJoonis(Kaart kaart){
-        Rectangle bg=new Rectangle(50,100);
-        bg.setArcHeight(20);
-        bg.setArcWidth(20);
-        bg.setFill(Color.WHITE);
+    public static void vasakValik(SeisuKontroll vasak, SeisuKontroll parem){
+        Stage vasakLava=new Stage();
+        Group juur=new Group();
+        int võitija = kumbVõidab(vasak,parem);
+        if(võitija==-1) {
+            mängijaVõitudeArv++;
+            juur.getChildren().add(new TextArea("Võitsid! seis on: \n Arvuti - "+arvutiVõitudeArv+ "\n Mängija - "+mängijaVõitudeArv));
 
-        Text tekst=new Text(kaart.getTugevus()+" "+kaart.getMast());
-        return new StackPane(bg,tekst);
+        }
+        if(võitija==1) {
+            arvutiVõitudeArv++;
+            juur.getChildren().add(new TextArea("Kaotasid! seis on: \n Arvuti - "+arvutiVõitudeArv+ "\n Mängija - "+mängijaVõitudeArv));
+        }
+        vasakLava.setScene(new Scene(juur));
+        vasakLava.show();
+
+    }
+
+    public static void paremValik(SeisuKontroll vasak, SeisuKontroll parem){
+        Stage paremLava=new Stage();
+        Group juur=new Group();
+        int võitija = kumbVõidab(vasak,parem);
+        if(võitija==1) {
+            mängijaVõitudeArv++;
+            juur.getChildren().add(new TextArea("Võitsid! seis on: \n Arvuti - "+arvutiVõitudeArv+ "\n Mängija - "+mängijaVõitudeArv));
+
+        }
+        if(võitija==-1) {
+            arvutiVõitudeArv++;
+            juur.getChildren().add(new TextArea("Kaotasid! seis on: \n Arvuti - "+arvutiVõitudeArv+ "\n Mängija - "+mängijaVõitudeArv));
+        }
+        paremLava.setScene(new Scene(juur));
+        paremLava.show();
 
     }
 }
--- Ryhmatoo2/src/main/java/com/example/ryhmatoo2/Mäng.java
+++ Ryhmatoo2/src/main/java/com/example/ryhmatoo2/Mäng.java
@@ -9,24 +9,6 @@ public class Mäng {
         int mängijaVõitudeArv = 0;
         int arvutiVõitudeArv = 0;
 
-        System.out.println();
-        System.out.println("Mängu eesmärgiks on valida kahe juhusliku pokkerikäe vahel. Pärast valiiku tegemist väljastatakse 5 ühiskaarti ja öeldakse kumb kätest võitis.");
-        System.out.println("Mängija võidu korral suurendatakse mängija punkte ühe võrra. Vastasel juhul suurenevad arvuti punktid. Kui käed olid sama tugevad, siis punktid jäävad samaks.");
-        System.out.println("Kaartide tugevuste arvestamisel vaadatakse maksimaalselt 5 kaarti korraga.");
-        System.out.println();
-        System.out.println("Pokkerikäte tugevusjärjestus (alates tugevaimast):");
-        System.out.println("Mastirida - 5 järjestikust kaarti, mis on samast mastist");
-        System.out.println("Nelik - 4 sama tugevusega kaarti");
-        System.out.println("Maja - kolmik ja paar");
-        System.out.println("Mast - 5 kaarti samast mastist");
-        System.out.println("Rida - 5 järjestikust kaarti");
-        System.out.println("Kolmik - 3 sama tugevusega kaarti");
-        System.out.println("Kaks paari");
-        System.out.println("Üks paar");
-        System.out.println("Kõrge kaart - Kõrgeim käesolev kaart.");
-        System.out.println();
-        System.out.println();
-
         while (true) {
             Pakk mängukaardid = new Pakk();
             mängukaardid.sega();
@@ -74,109 +56,9 @@ public class Mäng {
 
             System.out.println(SeisuKontroll.indeksSeisuks(vasak.tugevaimSeis()) + " ja " + SeisuKontroll.indeksSeisuks(parem.tugevaimSeis()));
 
-            //System.out.println("vasak");
-            //System.out.println(vasak.väljastaSeis());
-            //System.out.println("parem");
-            //System.out.println(parem.väljastaSeis());
 
-            int võitja = 0; //Kui vasak võidab, siis -1, viik 0, parem võidab 1.
 
-            if (parem.tugevaimSeis() < vasak.tugevaimSeis() && parem.tugevaimSeis() != -1) { // tugevama seisuga mängija võidab
-                System.out.println("Võidab parem, kellel on " + SeisuKontroll.indeksSeisuks(parem.tugevaimSeis()));
-                for (Kaart kaart : parem.tugevaimadViis()) {
-                    System.out.print(kaart + " ");
-                }
-                võitja = 1;
-            } else if (parem.tugevaimSeis() > vasak.tugevaimSeis() && vasak.tugevaimSeis() != -1) {
-                System.out.println("Võidab vasak, kellel on " + SeisuKontroll.indeksSeisuks(vasak.tugevaimSeis()));
-                for (Kaart kaart1 : vasak.tugevaimadViis()) {
-                    System.out.print(kaart1 + " ");
-                }
-                võitja = -1;
-            } else if (vasak.tugevaimSeis() == parem.tugevaimSeis()) { // kui on samad seisud, siis kontrollin kummal tugevam sama seis
-                int seis = vasak.tugevaimSeis();
-                List<Kaart> vasakKõigeTugevam = vasak.getSeisud().get(seis).get(vasak.getSeisud().get(seis).size() - 1); //kaartide list, mis moodustav kõige tugevama antud seisu
-                List<Kaart> paremKõigeTugevam = parem.getSeisud().get(seis).get(parem.getSeisud().get(seis).size() - 1);
-                if (seis == 2) {//võrdlen kahte maja, kumb tugevam
-                    List<List<Kaart>> vasakuMajaPaarJaKolmik = SeisuKontroll.majaPaarJaKolmik(vasakKõigeTugevam);
-                    List<List<Kaart>> paremaMajaPaarJaKolmik = SeisuKontroll.majaPaarJaKolmik(paremKõigeTugevam);
-                    int vasakuKolmikuTugevus = vasakuMajaPaarJaKolmik.get(0).get(0).getTugevusArv();
-                    int paremaKolmikuTugevus = paremaMajaPaarJaKolmik.get(0).get(0).getTugevusArv();
-                    int vasakuPaariTugevus = paremaMajaPaarJaKolmik.get(1).get(0).getTugevusArv();
-                    int paremaPaariTugevus = paremaMajaPaarJaKolmik.get(1).get(0).getTugevusArv();
-                    if (vasakuKolmikuTugevus > paremaKolmikuTugevus) {
-                        System.out.println("Võitja vasak!, tugevam maja");
-                        võitja = -1;
-                    } else if (vasakuKolmikuTugevus < paremaKolmikuTugevus) {
-                        System.out.println("Võitja parem!, tugevam maja");
-                        võitja = 1;
-                    } else if (vasakuPaariTugevus > paremaPaariTugevus) {
-                        System.out.println("Võitja vasak!, tugevam maja");
-                        võitja = -1;
-                    } else if (vasakuPaariTugevus < paremaPaariTugevus) {
-                        System.out.println("Võitja parem!, tugevam maja");
-                        võitja = 1;
-                    } else System.out.println("Viik!");
-                } else if (seis == 3) {//võrdlen kahte masti, kummal tugevam
-                    if (vasakKõigeTugevam.get(vasakKõigeTugevam.size() - 1).getTugevusArv() > paremKõigeTugevam.get(paremKõigeTugevam.size() - 1).getTugevusArv()) {
-                        System.out.println("Vasakul tugevam mast");
-                        võitja = -1;
-                    } else if (vasakKõigeTugevam.get(vasakKõigeTugevam.size() - 1).getTugevusArv() < paremKõigeTugevam.get(paremKõigeTugevam.size() - 1).getTugevusArv()) {
-                        System.out.println("Paremal tugevam mast");
-                        võitja = 1;
-                    } else System.out.println("Viik");
-                } else if (seis == 6) {// võrdlen kahte 2paari, kummal tugeval
-                    if (vasakKõigeTugevam.get(2).getTugevusArv() > paremKõigeTugevam.get(2).getTugevusArv()) {
-                        System.out.println("Vasakul kõrgem paar tugevam");
-                        võitja = -1;
-                    } else if (vasakKõigeTugevam.get(2).getTugevusArv() < paremKõigeTugevam.get(2).getTugevusArv()) {
-                        System.out.println("Paremal kõrgem paar tugevam");
-                        võitja = 1;
-                    } else if (vasakKõigeTugevam.get(0).getTugevusArv() > paremKõigeTugevam.get(0).getTugevusArv()) {
-                        System.out.println("Vasakul üks paaridest tugevam");
-                        võitja = -1;
-                    } else if (vasakKõigeTugevam.get(0).getTugevusArv() < paremKõigeTugevam.get(0).getTugevusArv()) {
-                        System.out.println("Paremal üks paaridest tugevam");
-                        võitja = 1;
-                    } else {
-                        if (vasak.getSeisud().get(8).get(0).get(0).getTugevusArv() > parem.getSeisud().get(8).get(0).get(0).getTugevusArv()) {
-                            System.out.println("Vasak võidab kõrgema kaardiga");
-                            võitja = -1;
-                        } else if (vasak.getSeisud().get(8).get(0).get(0).getTugevusArv() < parem.getSeisud().get(8).get(0).get(0).getTugevusArv()) {
-                            System.out.println("Parem võidab kõrgema kaardiga");
-                            võitja = 1;
-                        } else System.out.println("Viik!");
-                    }
-                } else {// ülejäänud seisude tugevust saab võrrelda kaartide tugevuste summana
-                    int vasakSumma = SeisuKontroll.tugevusedListist(vasakKõigeTugevam).stream().mapToInt(Integer::intValue).sum();
-                    int paremSumma = SeisuKontroll.tugevusedListist(paremKõigeTugevam).stream().mapToInt(Integer::intValue).sum();
-                    if (vasakSumma > paremSumma) {
-                        System.out.println("Vasak võidab tänu kõrgematele kaartidele");
-                        võitja = -1;
-                    } else if (vasakSumma < paremSumma) {
-                        System.out.println("Parem võidab tänu kõrgematele kaartidele");
-                        võitja = 1;
-
-                    } else if (seis == 4) { System.out.println("Viik");
-
-                    } else if (vasak.getSeisud().get(8).get(0).get(0).getTugevusArv() > parem.getSeisud().get(8).get(0).get(0).getTugevusArv()) {
-                        System.out.println("Vasak võidab kõrgema kaardiga");
-                        võitja = -1;
-                    } else if (vasak.getSeisud().get(8).get(0).get(0).getTugevusArv() < parem.getSeisud().get(8).get(0).get(0).getTugevusArv()) {
-                        System.out.println("Parem võidav kõrgema kaardiga");
-                        võitja = 1;
-                    } else if (seis == 5 || seis == 7) {
-                        if (vasak.getKäsi().get(0).getTugevusArv() > parem.getKäsi().get(0).getTugevusArv()) {
-                            System.out.println("Vasak võidab kõrgema käega");
-                            võitja = -1;
-                        } else if (vasak.getKäsi().get(0).getTugevusArv() < parem.getKäsi().get(0).getTugevusArv()) {
-                            System.out.println("Parem võidab kõrgema käega");
-                            võitja = 1;
-                        } else System.out.println("Viik");
-
-                    } else System.out.println("Viik");
-                }
-            }
+            int võitja=kumbVõidab(vasak,parem);
 
             if ((võitja == -1 && mängija.equals("vasak")) || (võitja == 1 && mängija.equals("parem")))
                 mängijaVõitudeArv++;
@@ -202,6 +84,108 @@ public class Mäng {
                 break;
             System.out.println();
         }
+    }
+
+    public static int kumbVõidab(SeisuKontroll vasak, SeisuKontroll parem){
+
+        if (parem.tugevaimSeis() < vasak.tugevaimSeis() && parem.tugevaimSeis() != -1) { // tugevama seisuga mängija võidab
+            System.out.println("Võidab parem, kellel on " + SeisuKontroll.indeksSeisuks(parem.tugevaimSeis()));
+            for (Kaart kaart : parem.tugevaimadViis()) {
+                System.out.print(kaart + " ");
+            }
+            return 1;
+        } else if (parem.tugevaimSeis() > vasak.tugevaimSeis() && vasak.tugevaimSeis() != -1) {
+            System.out.println("Võidab vasak, kellel on " + SeisuKontroll.indeksSeisuks(vasak.tugevaimSeis()));
+            for (Kaart kaart1 : vasak.tugevaimadViis()) {
+                System.out.print(kaart1 + " ");
+            }
+            return -1;
+        } else if (vasak.tugevaimSeis() == parem.tugevaimSeis()) { // kui on samad seisud, siis kontrollin kummal tugevam sama seis
+            int seis = vasak.tugevaimSeis();
+            List<Kaart> vasakKõigeTugevam = vasak.getSeisud().get(seis).get(vasak.getSeisud().get(seis).size() - 1); //kaartide list, mis moodustav kõige tugevama antud seisu
+            List<Kaart> paremKõigeTugevam = parem.getSeisud().get(seis).get(parem.getSeisud().get(seis).size() - 1);
+            if (seis == 2) {//võrdlen kahte maja, kumb tugevam
+                List<List<Kaart>> vasakuMajaPaarJaKolmik = SeisuKontroll.majaPaarJaKolmik(vasakKõigeTugevam);
+                List<List<Kaart>> paremaMajaPaarJaKolmik = SeisuKontroll.majaPaarJaKolmik(paremKõigeTugevam);
+                int vasakuKolmikuTugevus = vasakuMajaPaarJaKolmik.get(0).get(0).getTugevusArv();
+                int paremaKolmikuTugevus = paremaMajaPaarJaKolmik.get(0).get(0).getTugevusArv();
+                int vasakuPaariTugevus = paremaMajaPaarJaKolmik.get(1).get(0).getTugevusArv();
+                int paremaPaariTugevus = paremaMajaPaarJaKolmik.get(1).get(0).getTugevusArv();
+                if (vasakuKolmikuTugevus > paremaKolmikuTugevus) {
+                    System.out.println("Võitja vasak!, tugevam maja");
+                    return -1;
+                } else if (vasakuKolmikuTugevus < paremaKolmikuTugevus) {
+                    System.out.println("Võitja parem!, tugevam maja");
+                    return 1;
+                } else if (vasakuPaariTugevus > paremaPaariTugevus) {
+                    System.out.println("Võitja vasak!, tugevam maja");
+                    return -1;
+                } else if (vasakuPaariTugevus < paremaPaariTugevus) {
+                    System.out.println("Võitja parem!, tugevam maja");
+                    return 1;
+                } else System.out.println("Viik!");
+            } else if (seis == 3) {//võrdlen kahte masti, kummal tugevam
+                if (vasakKõigeTugevam.get(vasakKõigeTugevam.size() - 1).getTugevusArv() > paremKõigeTugevam.get(paremKõigeTugevam.size() - 1).getTugevusArv()) {
+                    System.out.println("Vasakul tugevam mast");
+                    return -1;
+                } else if (vasakKõigeTugevam.get(vasakKõigeTugevam.size() - 1).getTugevusArv() < paremKõigeTugevam.get(paremKõigeTugevam.size() - 1).getTugevusArv()) {
+                    System.out.println("Paremal tugevam mast");
+                    return 1;
+                } else System.out.println("Viik");
+            } else if (seis == 6) {// võrdlen kahte 2paari, kummal tugeval
+                if (vasakKõigeTugevam.get(2).getTugevusArv() > paremKõigeTugevam.get(2).getTugevusArv()) {
+                    System.out.println("Vasakul kõrgem paar tugevam");
+                    return -1;
+                } else if (vasakKõigeTugevam.get(2).getTugevusArv() < paremKõigeTugevam.get(2).getTugevusArv()) {
+                    System.out.println("Paremal kõrgem paar tugevam");
+                    return 1;
+                } else if (vasakKõigeTugevam.get(0).getTugevusArv() > paremKõigeTugevam.get(0).getTugevusArv()) {
+                    System.out.println("Vasakul üks paaridest tugevam");
+                    return -1;
+                } else if (vasakKõigeTugevam.get(0).getTugevusArv() < paremKõigeTugevam.get(0).getTugevusArv()) {
+                    System.out.println("Paremal üks paaridest tugevam");
+                    return 1;
+                } else {
+                    if (vasak.getSeisud().get(8).get(0).get(0).getTugevusArv() > parem.getSeisud().get(8).get(0).get(0).getTugevusArv()) {
+                        System.out.println("Vasak võidab kõrgema kaardiga");
+                        return -1;
+                    } else if (vasak.getSeisud().get(8).get(0).get(0).getTugevusArv() < parem.getSeisud().get(8).get(0).get(0).getTugevusArv()) {
+                        System.out.println("Parem võidab kõrgema kaardiga");
+                        return 1;
+                    } else System.out.println("Viik!");
+                }
+            } else {// ülejäänud seisude tugevust saab võrrelda kaartide tugevuste summana
+                int vasakSumma = SeisuKontroll.tugevusedListist(vasakKõigeTugevam).stream().mapToInt(Integer::intValue).sum();
+                int paremSumma = SeisuKontroll.tugevusedListist(paremKõigeTugevam).stream().mapToInt(Integer::intValue).sum();
+                if (vasakSumma > paremSumma) {
+                    System.out.println("Vasak võidab tänu kõrgematele kaartidele");
+                    return -1;
+                } else if (vasakSumma < paremSumma) {
+                    System.out.println("Parem võidab tänu kõrgematele kaartidele");
+                    return 1;
+
+                } else if (seis == 4) {
+                    System.out.println("Viik");
+
+                } else if (vasak.getSeisud().get(8).get(0).get(0).getTugevusArv() > parem.getSeisud().get(8).get(0).get(0).getTugevusArv()) {
+                    System.out.println("Vasak võidab kõrgema kaardiga");
+                    return -1;
+                } else if (vasak.getSeisud().get(8).get(0).get(0).getTugevusArv() < parem.getSeisud().get(8).get(0).get(0).getTugevusArv()) {
+                    System.out.println("Parem võidav kõrgema kaardiga");
+                    return 1;
+                } else if (seis == 5 || seis == 7) {
+                    if (vasak.getKäsi().get(0).getTugevusArv() > parem.getKäsi().get(0).getTugevusArv()) {
+                        System.out.println("Vasak võidab kõrgema käega");
+                        return -1;
+                    } else if (vasak.getKäsi().get(0).getTugevusArv() < parem.getKäsi().get(0).getTugevusArv()) {
+                        System.out.println("Parem võidab kõrgema käega");
+                        return 1;
+                    } else System.out.println("Viik");
+
+                } else System.out.println("Viik");
+            }
+        }
+        return 0;
     }
 
     public static String väljastaKaardid(List<Kaart> kaardid) {
